ed9cbc14dca79987d28962d371e22022bd300130
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-04-23 09:05:27 +0200

    refactor: Modificar metodo toString()

--- app/src/main/java/edu/teamrocket/brunoxBoxing/ScoreCard.java
+++ app/src/main/java/edu/teamrocket/brunoxBoxing/ScoreCard.java
@@ -36,10 +36,58 @@ public class ScoreCard {
 
     @Override   
     public String toString() {
-        return    "\n\t\t\t" + this.color 
-                + "\n\t" + this.redCorner
-                + "\t" + this.blueCorner
-                + "\nRound\tScore\tRound\tRound\tScore"
-                + "\nScore\tTotal\t\tTotal\tScore";
+        return    "\n\t\t\t   " + this.color 
+                + "\n\t\t" + this.blueCorner 
+                + "\t" + this.redCorner 
+                + "\n\t\t\t"
+                + this.getNumRounds() + " rounds\n" 
+                + this.viewRounds() 
+                + "\n\t FINAL SCORE: "
+                + this.getRedBoxerFinalScore() 
+                + " - " 
+                + this.getBlueBoxerFinalScore() 
+                + " FINAL SCORE";
     }       
+
+    public int getRedBoxerFinalScore() {
+        int redBoxerScore = 0;
+        for (Round round : rounds) {
+            redBoxerScore += round.getRedBoxerScore();
+        }
+        return redBoxerScore;
+    }
+
+    public int getBlueBoxerFinalScore() {
+        int blueBoxerScore = 0;
+        for (Round round : rounds) {
+            blueBoxerScore += round.getRedBoxerScore();
+        }
+        return blueBoxerScore;
+    }
+
+
+    private String viewRounds() {
+        StringBuilder text = new StringBuilder();
+
+        text.append("\nRound\tScore\tRound\tRound\tScore")
+            .append("\nScore\tTotal\t\tTotal\tScore");
+
+        byte roundNum = 1;
+        byte redBoxerScoreTotal = 0;
+        byte blueBoxerScoreTotal = 0;
+
+        for (Round round : this.rounds) {
+            text.append("\n\t")
+                .append(round.getRedBoxerScore())
+                .append("\t\s")
+                .append(redBoxerScoreTotal += round.getRedBoxerScore())
+                .append("\t\s\s")
+                .append(roundNum++)
+                .append("\t\s")
+                .append(blueBoxerScoreTotal += round.getBlueBoxerScore())
+                .append("\t\s")
+                .append(round.getBlueBoxerScore());
+        }
+        return text.toString();
+    }
 }
